3086318453ea8a55e3a4c7f4143ce7d81e33a461
seshanb <125367985+seshanb@users.noreply.github.com>
2023-09-29 12:03:41 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "virtual_machines" {
-  value = data.google_compute_instance.vms.*.self_link
+output "instance_name" {
+  value = google_compute_instance.example_instance.name
 }
 
-output "virtual_machine_images" {
-  value = data.google_compute_image.images.*.self_link
+output "instance_ip" {
+  value = google_compute_instance.example_instance.network_interface.0.access_config.0.assigned_nat_ip
 }
