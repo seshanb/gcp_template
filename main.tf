b183918df2428e3b76c713da87c01963db2a9564
seshanb <125367985+seshanb@users.noreply.github.com>
2023-09-29 12:04:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "example_instance" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
