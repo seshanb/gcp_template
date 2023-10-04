48ea444274da73c316ba7f8ed29ed14c57b9c8c2
seshanb <125367985+seshanb@users.noreply.github.com>
2023-10-04 17:15:36 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
 output "instance_name" {
   value = google_compute_instance.example_instance.name
 }
-
-output "instance_ip" {
-  value = google_compute_instance.example_instance.network_interface.0.access_config.0.assigned_nat_ip
-}
